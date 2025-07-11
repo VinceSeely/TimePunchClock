8cc94f1147b31041c1978a5252b6d1e33c50c97a
VinceSeely <vince@vinceseely.com>
2025-07-11 15:44:26 -0500

    update api to work

--- src/TimeApi/Api/TimePunchController.cs
+++ src/TimeApi/Api/TimePunchController.cs
@@ -1,25 +1,37 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using TimeApi.Services;
 using TimeClock.Client;
 
 namespace TimeApi.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TimePunchController() : ControllerBase
+    public class TimePunchController(TimePunchRepository punchRepository) : ControllerBase
     {
 
 
         [HttpGet("hours")]
         public ActionResult GetHours(DateTime start, DateTime end)
         {
-            throw new NotImplementedException();
+            var results = punchRepository.GetPunchRecords(start, end);
+            if (!results.IsNullOrEmpty())
+            {
+                return BadRequest("No results found for date range");
+            }
+            return Ok(results);
         }
 
         [HttpPost()]
         public ActionResult PunchHours(PunchInfo punchInfo)
         {
-            throw new NotImplementedException();
+            punchRepository.InsertPunch(punchInfo);
+
+            return Ok();
+
         }
     }
 
